327858748e339311f4468b080071e15f15a8444c
Brian Wong <bwong92@gmail.com>
2019-11-26 20:15:11 -0500

    OrderItemsController

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,4 +1,16 @@
 class OrderItemsController < ApplicationController
-
+    def index
+        render json: OrderItem.all
+    end
     
+    def create
+        OrderItem.create(order_item_params)
+    end
+
+    private
+
+    def order_item_params
+        params.permit(:order_id, :item_id)
+    end
+
 end
