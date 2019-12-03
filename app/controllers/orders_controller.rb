ba0a4aa219ad7aed7e357a004e4f5ae6787e0fed
Brian Wong <bwong92@gmail.com>
2019-12-03 13:07:11 -0500

    cart backend update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,15 +14,20 @@ class OrdersController < ApplicationController
         encoded_token = request.headers['Authorization'].split(' ')[1]
         token = decoded_token = JWT.decode(encoded_token, secret , true, {algorithm: 'HS256'})
         user_id = token[0]['user_id']
+        byebug
+        cur_user = User.find_by(id: user_id)
+        cur_user_orders = cur_user.orders
+        cart = cur_user_orders.find_by(cart: true)
+        cart.update(cart: false)
+
+        new_cart = Order.create(cart: true, user_id: user_id)
         
-        order = Order.create(cart: false, user_id: user_id)
+        # params[:order][:cart].each do |item_in_cart|
+        #     item = Item.find(item_in_cart[:id])
+        #     orderItems = OrderItem.create(order_id: order[:id], item_id: item.id )
+        # end
         
-        params[:order][:cart].each do |item_in_cart|
-            item = Item.find(item_in_cart[:id])
-            orderItems = OrderItem.create(order_id: order[:id], item_id: item.id )
-        end
-        
-        render json: order
+        render json: cur_user, include: "**"
        
     end
 
