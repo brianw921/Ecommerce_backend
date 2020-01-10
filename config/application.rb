e767c0a64968bd75f56b96e60ab85cbf475dba4d
Brian Wong <bwong92@gmail.com>
2020-01-10 17:04:57 -0500

    heroku rails backend

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module EcommerceBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults "6.0"
     config.autoloader = :classic
-    config.eager_load_paths -= %W(#{config.root}/app/overrides)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
